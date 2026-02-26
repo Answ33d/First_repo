6cd743656f6fa121080a30c0615223d12fb01077
answ33d <answ33d@gmail.com>
2026-02-26 21:23:02 +0200

    third commit

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("hello world");
-print("hello Git");
+item=input("Product name:")
+quantity=int(input("quantity: "))
+price=float(input("Price"))
+discount=float(input("Discount in %"))
+
+print(f"You bought {quantity} {item.upper()} for {price*quantity} USD . Your discount is {discount}% . To pay {(price-(price/100*discount))*quantity}")
